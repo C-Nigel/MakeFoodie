514af4b84b5ea4238d315d7254921bb69a65270f
Nigel Cheong <Nigel_cheong@yahoo.com.sg>
2020-05-13 13:46:29 +0800

    Initial Commit

---
+++ MakeFoodie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MakeFoodie
+//
+//  Created by Nigel Cheong on 13/5/20.
+//  Copyright © 2020 ITP312. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
