09fdf5dd2718f23b90dc976c93bd494a8725ec8c
NIgel Cheong <nigel_cheong@yahoo.com.sg>
2020-07-07 22:11:30 +0800

    added database bridging headers and data manager files

---
+++ MakeFoodie/Views/Database/Bridging Header/BridgingHeader.m
@@ -0,0 +1,13 @@
+//
+//  BridgingHeader.m
+//  MakeFoodie
+//
+//  Created by NIgel Cheong on 7/7/20.
+//  Copyright © 2020 ITP312. All rights reserved.
+//
+
+#import "BridgingHeader.h"
+
+@implementation BridgingHeader
+
+@end
