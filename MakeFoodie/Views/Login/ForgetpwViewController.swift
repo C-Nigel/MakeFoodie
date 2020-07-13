d5e44497b2893b2273ffce35eed3a27e7791bafe
Raymond <raymondsinglaire4@gmail.com>
2020-07-13 16:01:40 +0800

    forgeet pw done plus others..

---
+++ MakeFoodie/Views/Login/ForgetpwViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ForgetpwViewController.swift
+//  MakeFoodie
+//
+//  Created by ITP312Grp2 on 13/7/20.
+//  Copyright © 2020 ITP312. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseFirestoreSwift
+import FirebaseFirestore
+
+class ForgetpwViewController: UIViewController {
+
+    @IBOutlet weak var emailinput: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func sendemail(_ sender: Any) {
+        Auth.auth().sendPasswordReset(withEmail: emailinput.text!) { (error) in
+            if error != nil{
+                print("Error sending email!")
+            }
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
