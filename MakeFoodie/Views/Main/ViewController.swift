e340a0ab2ce742ad931e763701b96b6f74e600bb
Raymond <raymondsinglaire@gmail.com>
2020-07-21 07:26:35 -0700

    fixed image error in profile, aded notification when user create account and follower count in profile not hardcoded

--- MakeFoodie/Views/Main/ViewController.swift
+++ MakeFoodie/Views/Main/ViewController.swift
@@ -7,11 +7,19 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
-
+    var isGrantedNotificationAccess = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            self.isGrantedNotificationAccess = granted
+            if !granted{
+                //add alert to complain to user
+            }
+        }
         // Do any additional setup after loading the view.
         
     }
