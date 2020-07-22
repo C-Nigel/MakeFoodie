5b145ba95536301b665905faffd9d37603c2667f
NIgel Cheong <nigel_cheong@yahoo.com.sg>
2020-07-22 11:25:36 +0800

    added validation for following post and recipes

--- MakeFoodie/Views/Database/DataManager/DataManager.swift
+++ MakeFoodie/Views/Database/DataManager/DataManager.swift
@@ -186,6 +186,7 @@ class DataManager: NSObject {
     
     static func loadFollowPostItems(onComplete: (([postDetails]) -> Void)?)
     {
+        let userUID = Auth.auth().currentUser?.uid
         var followList : [Follow] = []
         var postItems : [postDetails] = []
         
@@ -212,7 +213,7 @@ class DataManager: NSObject {
                     
                     if item != nil
                     {
-                        if item?.type == "post"
+                        if item?.type == "post" && item?.followeruid == userUID
                         {
                             followList.append(item!)
                         }
@@ -260,6 +261,7 @@ class DataManager: NSObject {
     
     static func loadFollowRecipeItems(onComplete: (([recipeDetails]) -> Void)?)
     {
+        let userUID = Auth.auth().currentUser?.uid
         var followList : [Follow] = []
         var recipeItems : [recipeDetails] = []
         
@@ -286,7 +288,7 @@ class DataManager: NSObject {
                     
                     if item != nil
                     {
-                        if item?.type == "recipes"
+                        if item?.type == "recipes" && item?.followeruid == userUID
                         {
                             followList.append(item!)
                         }
